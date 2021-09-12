8fd0b6a2024ac5eb3b64112a01a3df2e31f0af7e
Krystian Brazulewicz <krystian@brazulewicz.pl>
2021-09-13 00:24:55 +0200

    Time Conversion

---
+++ time-conversion/src/Solution.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+class Result {
+
+    final static DateTimeFormatter DTF_INPUT = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendPattern("hh:mm:ssa")
+            .toFormatter();
+
+    final static DateTimeFormatter DTF_OUTPUT = new DateTimeFormatterBuilder()
+            .appendPattern("HH:mm:ss")
+            .toFormatter();
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        // Write your code here
+        return LocalTime.parse(s, DTF_INPUT).format(DTF_OUTPUT);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
