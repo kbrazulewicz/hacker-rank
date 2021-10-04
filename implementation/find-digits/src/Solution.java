c35f8f907e98382b7581d7c15ccfed76c18934c8
Krystian Brazulewicz <krystian@brazulewicz.pl>
2021-10-04 19:53:31 +0200

    Find Digits

---
+++ implementation/find-digits/src/Solution.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        // Write your code here
+        int a = n;
+        int result = 0;
+
+        while (a != 0) {
+            int b = a % 10;
+            if (b != 0 && n % b == 0) result++;
+            a /= 10;
+        }
+
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
