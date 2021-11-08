44580d7b809cd4a844a272dceda9d3f5b8c64145
Krystian Brazulewicz <krystian@brazulewicz.pl>
2021-11-08 23:29:31 +0100

    Manasa and stones

---
+++ manasa-and-stones/src/Solution.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+
+class Result {
+
+    /*
+     * Complete the 'stones' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER a
+     *  3. INTEGER b
+     */
+
+    public static List<Integer> stones(int n, int a, int b) {
+        // Write your code here
+        int lastStone = n - 1;
+        return IntStream.rangeClosed(0, lastStone)
+                .map(i -> i * b + (lastStone - i) * a)
+                .sorted()
+                .distinct()
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int a = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> result = Result.stones(n, a, b);
+
+                bufferedWriter.write(
+                        result.stream()
+                                .map(Object::toString)
+                                .collect(joining(" "))
+                                + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
