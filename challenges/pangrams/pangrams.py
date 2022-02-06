281407ae4af7e8a314e5825be38a045c87d88810
Krystian Brazulewicz <kbrazulewicz@atlassian.com>
2022-02-06 21:52:39 +0100

    Pangrams

---
+++ challenges/pangrams/pangrams.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    letters = {c for c in 'abcdefghijklmnopqrstuvwxyz'}
+
+    for c in s.lower():
+        letters.discard(c)
+
+    return 'pangram' if letters.__len__() == 0 else 'not pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
